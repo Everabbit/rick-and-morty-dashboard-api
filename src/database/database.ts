bfc484acc1074d4a09544a85ce4443832efef3bc
Everabbit <wendeldelemoscoelho@gmail.com>
2026-04-22 20:51:50 -0300

    remoção de sync no database

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,9 +1,7 @@
-import { env } from "../config/env.js";
 import { sequelize } from "./sequelize.js";
 import { initModels } from "../models/index.model.js";
 
 export const ensureDatabaseReady = async (): Promise<void> => {
   initModels();
   await sequelize.authenticate();
-  await sequelize.sync({ alter: env.nodeEnv === "development" });
 };
